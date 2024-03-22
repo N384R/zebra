0dbb997a4a0631f146c72cfc6f46efca5dc8865e
N384R <juu8242@naver.com>
2024-03-22 14:48:09 +0900

    mod

--- test.py
+++ test.py
@@ -6,22 +6,18 @@ print("Test case 1:")
 mol1 = np.eye(3)
 qc = zebra.QuantumCalculator()
 energy1, eigenvectors1 = qc.hartree_fock(mol1)
-qc.print_results()
 
 # Test case 2: Random matrix
 print("\nTest case 2:")
 mol2 = np.random.rand(3, 3)
 energy2, eigenvectors2 = qc.hartree_fock(mol2)
-qc.print_results()
 
 # Test case 3: Zero matrix
 print("\nTest case 3:")
 mol3 = np.zeros((3, 3))
 energy3, eigenvectors3 = qc.hartree_fock(mol3)
-qc.print_results()
 
 # Test case 4: H2 molecule
 print("\nTest case 4:")
 mol4 = np.array([[1.0, 0.5], [0.5, 1.0]])
 energy4, eigenvectors4 = qc.hartree_fock(mol4)
-qc.print_results()
