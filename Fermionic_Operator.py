b7b6514df3a501a281dbff497289a33bcb08ccb4
N384R <juu8242@naver.com>
2024-03-31 01:09:24 +0900

    mod

--- Fermionic_Operator.py
+++ Fermionic_Operator.py
@@ -136,3 +136,5 @@ print("in the order of the qubit operators.\n")
 print("Rule: [value] [operator] [operator] ...\n(^ is indicates the dagger operator)")
 input_operator = input("Input: ")
 sort_operator(input_operator)
+
+print('hello')
