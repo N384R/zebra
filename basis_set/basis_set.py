b6a84038bd3a9869d7273ba2d01fd2638b03a160
N384R <juu8242@naver.com>
2024-04-21 22:08:31 +0900

    modi

--- basis_set/basis_set.py
+++ basis_set/basis_set.py
@@ -28,22 +28,22 @@ class BasisSet:
                 data = line.split()
 
                 if data[0] == '!':
-                    element = data[1]
-                    basis_set_dict[element] = {}
+                    element, charge = data[1], data[2]
+                    basis_set_dict[element] = {'charge': charge, 'basis': {}}
                 elif data[0].isalpha():
                     orbital_type = data[0]
                     if orbital_type not in basis_set_dict[element]:
-                        basis_set_dict[element][orbital_type] = []
+                        basis_set_dict[element]['basis'][orbital_type] = []
                 else:
-                    basis_set_dict[element][orbital_type].append(list(map(float, data)))
+                    basis_set_dict[element]['basis'][orbital_type].append(list(map(float, data)))
 
         return basis_set_dict
 
     def show_basis_set(self):
         '''Print the basis set.'''
         print(f"Basis set: {self.basis_set_name}")
-        for element, basis_function in self.basis_set.items():
-            print(f"{element}: {basis_function}")
+        for element, info in self.basis_set.items():
+            print(f"{element} (Charge: {info['charge']}): {info['basis']}")
 
     def __str__(self):
         return f"Basis set: {self.basis_set_name}"
